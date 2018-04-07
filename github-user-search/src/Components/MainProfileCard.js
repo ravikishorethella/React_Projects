b49c125e1f15df9f60fb0d106619c8a30cf1b375
Ravi Kishore Thella <ravikishorethella@gmail.com>
2018-04-07 18:53:14 -0400

    added github-user-search project

---
+++ github-user-search/src/Components/MainProfileCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class MainProfileCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: 'ravikishorethella',
+            name: '',
+            picture: '',
+            location: '',
+            email: '',
+            blog: '',
+            repos: '',
+            followers: '',
+            following: ''
+        }
+    }
+}
