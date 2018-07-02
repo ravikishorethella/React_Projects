861ece67cff88ec0cd0ec067df754f46dbc7d85f
RaviKThella <ravikthella@pop-os.localdomain>
2018-07-02 16:17:28 -0400

    testing the components

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,9 +1,78 @@
 import React, { Component } from "react";
 import "./App.css";
 
+// Header component
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <h3>{this.props.subtitle}</h3>
+      </div>
+    );
+  }
+}
+
+// Action component
+
+class Action extends Component {
+  render() {
+    return (
+      <div>
+        <button>Do something</button>
+      </div>
+    );
+  }
+}
+
+// Options component
+class Options extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.options.map(option => (
+          <Option key={option} optionText={option} />
+        ))}
+      </div>
+    );
+  }
+}
+
+// Option component
+class Option extends Component {
+  render() {
+    return <div>{this.props.optionText}</div>;
+  }
+}
+
+// Add Option component
+class AddOption extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is an add option component</p>
+        <form>
+          <input type="text" />
+          <button>Add option</button>
+        </form>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
-    return <div>set up</div>;
+    const title = "To-Do App";
+    const subtitle = "change is good";
+    const options = ["option 1", "option 2", "option 3"];
+    return (
+      <div>
+        <Header title={title} subtitle={subtitle} />
+        <Action />
+        <Options options={options} />
+        <AddOption />
+      </div>
+    );
   }
 }
 
