97fd8dd769dd520ed01d07fb720422f2ef3a2c7f
RaviKThella <ravikthella@pop-os.localdomain>
2018-07-02 11:52:50 -0400

    youtube-app

---
+++ youtube-app/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./App.css";
+
+let x;
+
+class App extends Component {
+  render() {
+    return <div>Setup complete</div>;
+  }
+}
+
+export default App;
