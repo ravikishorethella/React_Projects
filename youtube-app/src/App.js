a8d658639d21e69591aa629553ca2f96538f5946
RaviKThella <ravikthella@pop-os.localdomain>
2018-07-06 16:56:42 -0400

    youtube-app added

---
+++ youtube-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import SearchBar from './components/SearchBar'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div> New App
+        <SearchBar/>
+      </div>
+    );
+  }
+}
+
+export default App;
